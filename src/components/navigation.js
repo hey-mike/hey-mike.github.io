d7b3e60f18b25b97fb85b9c85c69f0f53cee98ac
Michael Luo <michael.luo0801@gmail.com>
2021-08-28 21:48:55 +1000

    add github and linkedin

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import { Github, LinkedinIn } from '@styled-icons/fa-brands'
 import * as styles from './navigation.module.css'
 
 const Navigation = () => (
@@ -18,6 +18,19 @@ const Navigation = () => (
       <li className={styles.navigationItem}>
         <Link to="/blog/" activeClassName="active">
           Blog
+        </Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to="https://github.com/hey-mike" activeClassName="active">
+          <Github size={28} />
+        </Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link
+          to="https://www.linkedin.com/in/michael-luo-10066110a/"
+          activeClassName="active"
+        >
+          <LinkedinIn size={28} />
         </Link>
       </li>
     </ul>
